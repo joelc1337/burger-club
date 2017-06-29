58bda3ca997e89fac29952ae87d485ee76bfd5f7
sonicthegamer <joelcolon1337@Gmail.com>
2017-06-29 20:16:20 +0000

    final done

--- script.js
+++ script.js
@@ -2,9 +2,10 @@ $(document).ready(function() {
 
 
 $("#myBtn").click(function(){
- var email = $("#mail").val();
- var names = $("#name").val();        
-
+ var email = $("#email").val();
+ var names = $("#name").val();      
+ 
+ $(".modal-body").append("Hi " + names + " thank you for signing up! We will send an email to " + email + " shortly")
 })
 
 
